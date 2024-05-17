47d63a1a648988ebbbe1ddc801ed214ffb5010b2
Keshav <keshav.ram@live.fr>
2024-05-17 23:55:54 +0200

    levelone

---
+++ src/main/java/com/keshav/levelone/Game.java
@@ -0,0 +1,13 @@
+package com.keshav.levelone;
+
+import java.util.Timer;
+
+public class Game {
+    public boolean gameOver;
+    public Timer timer;
+
+    public void win(Timer time, boolean isBoss) {
+
+        
+    }
+}
